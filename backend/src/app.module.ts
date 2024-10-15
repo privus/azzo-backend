58143433a34dde8bcaef78f9c0748a1a33804bf0
andrejvb <andre.jvb@gmail.com>
2024-10-15 15:27:29 -0300

    instala npm, dockercompose, dockerfile

---
+++ backend/src/app.module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './users/user.entity';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DATABASE_HOST || 'localhost',
+      port: parseInt(process.env.DATABASE_PORT, 10) || 3306,
+      username: process.env.DATABASE_USER || 'usuario',
+      password: process.env.DATABASE_PASSWORD || 'senha',
+      database: process.env.DATABASE_NAME || 'meu_banco',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true, // Somente para desenvolvimento
+    }),
+    TypeOrmModule.forFeature([User]),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
