b821199ab133ca8cf9fb468d8200f980d8fb4baa
andrejvb <andre.jvb@gmail.com>
2024-11-07 18:07:20 -0300

    Aws env24

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,24 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger, ValidationPipe } from '@nestjs/common';
-import * as cors from 'cors';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const logger = new Logger('Bootstrap');
 
-  const allowedOrigins = ['http://localhost:4200', 'http://172.18.0.4:4200'];
+  // Domínios permitidos
+  const allowedOrigins = [
+    'http://localhost:4200', // Para desenvolvimento local
+    'http://172.18.0.4:4200', // Endereço interno do container
+    'http://54.233.159.54', // Endereço do frontend de produção
+  ];
 
-  // Configurar CORS
-  app.use(
-    cors({
-      origin: allowedOrigins,
-      credentials: true,
-    }),
-  );
+  // Configuração do CORS
+  app.enableCors({
+    origin: allowedOrigins,
+    credentials: true,
+  });
 
+  // Ativar validação global
   app.useGlobalPipes(new ValidationPipe());
+
   // Configuração do Swagger (opcional)
   const config = new DocumentBuilder()
     .setTitle('API Azzo')
@@ -29,8 +33,11 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api-docs', app, document);
 
-  await app.listen(process.env.PORT || 3000);
-  logger.log(`Aplicação rodando em: http://localhost:${process.env.PORT || 3000}`);
-  logger.log(`Swagger disponível em: http://localhost:${process.env.PORT || 3000}/api-docs`);
+  // Iniciar aplicação
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
+  logger.log(`Aplicação rodando em: http://localhost:${port}`);
+  logger.log(`Swagger disponível em: http://localhost:${port}/api-docs`);
 }
+
 bootstrap();
