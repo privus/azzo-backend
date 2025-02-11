9ec1a1e95df9915d3bfbdfa96e191a55fea514a6
andrejvb <andre.jvb@gmail.com>
2025-02-11 11:49:37 -0300

    configs para mongoDB 12

--- src/infrastructure/database/seeds/estadoSeed.ts
+++ src/infrastructure/database/seeds/estadoSeed.ts
@@ -14,9 +14,7 @@ export class EstadoSeed implements Seeder {
 
     // Itera sobre os dados e insere no banco de dados
     for (const estado of jsonData) {
-      const estadoExists = await estadoRepository.findOne({
-        where: { nome: estado.nome }
-      });
+      const estadoExists = await estadoRepository.findOneBy({ nome: estado.nome });
 
       if (estadoExists) {
         console.log(`Estado ${estado.nome} já existe. Nenhuma alteração foi feita.`);
