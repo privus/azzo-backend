a7afed13873f099c82b28e80c937ddd720a0740a
andrejvb <andre.jvb@gmail.com>
2025-02-12 14:23:11 -0300

    config wordpane

--- src/infrastructure/database/data-source.ts
+++ src/infrastructure/database/data-source.ts
@@ -2,11 +2,11 @@ import { DataSource } from 'typeorm';
 
 export const AppDataSource = new DataSource({
   type: 'mysql',
-  host: process.env.DB_HOST || 'localhost',
+  host: process.env.DB_HOST || '152.53.39.254',
   port: parseInt(process.env.DB_PORT, 10) || 3306,
-  username: process.env.DB_USERNAME || 'usuario',
-  password: process.env.DB_PASSWORD || 'senha',
-  database: process.env.DB_NAME || 'meu_banco',
+  username: process.env.DB_USERNAME || 'azzo-user',
+  password: process.env.DB_PASSWORD || 'privus123',
+  database: process.env.DB_NAME || 'azzo-database',
   entities: [`${__dirname}**/entities/*.{ts,js}`],
   migrations: ['dist/infrastructure/database/migrations/*.js'],
   synchronize: false, // Defina como false em produção e use migrações
