b2e92439b7e76f24915cdf81378c98b6c393877e
andrejvb <andre.jvb@gmail.com>
2025-06-27 10:55:26 -0300

    fix: desliga synchronize

--- src/infrastructure/database/entities/pStatusVenda.ts
+++ src/infrastructure/database/entities/pStatusVenda.ts
@@ -1,14 +1,15 @@
 import { Entity, Column, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
-import { Venda } from './';
+import { PVenda, Venda } from './';
 
 @Entity('p_status_venda')
-export class PStatusVenda {
+export class StatusVenda {
   @PrimaryGeneratedColumn('increment')
   status_venda_id: number;
 
   @Column({ type: 'varchar', length: 45 })
   nome: string;
 
-  @OneToMany(() => Venda, (venda) => venda.status_venda)
-  vendas: Venda[];
+  @OneToMany(() => PVenda, (venda) => venda.status_venda)
+  vendas: PVenda[];
 }
+
