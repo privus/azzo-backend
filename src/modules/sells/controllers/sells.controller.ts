ae1b41a2b203d6c4aaa69507e3d9393127b7cd84
andrejvb <andre.jvb@gmail.com>
2025-08-15 14:02:42 -0300

    fix: ajusta parametros metodo groupConsumption2

--- src/modules/sells/controllers/sells.controller.ts
+++ src/modules/sells/controllers/sells.controller.ts
@@ -35,8 +35,8 @@ export class SellsController {
 
   @ApiOperation({ summary: 'Relatorio de vendas por grupo clientes' })
   @Get('groupCustomers')
-  async sellsByGroupCustomers(@Query('groupId') grupoId: number, @Query('supplierId') supplierId: number, @Query('fromDate') fromDate: string, @Query('toDate') toDate?: string) {
-    return this.sellsService.groupConsumption({ grupoId, supplierId, fromDate, toDate })
+  async sellsByGroupCustomers(@Query('groupId') groupId: number, @Query('supplierId') supplierId: number, @Query('fromDate') fromDate: string, @Query('toDate') toDate?: string) {
+    return this.sellsService.groupConsumption({ groupId, supplierId, fromDate, toDate })
   }
 
   @ApiOperation({ summary: 'Obter ranking de vendas do dia' })
