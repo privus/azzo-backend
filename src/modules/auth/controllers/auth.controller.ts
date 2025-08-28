4d49bad36c0e3c1f391441e02e5e612f32580864
andrejvb <andre.jvb@gmail.com>
2025-08-28 17:01:16 -0300

    feat: retira gerador da rota tokens bling

--- src/modules/auth/controllers/auth.controller.ts
+++ src/modules/auth/controllers/auth.controller.ts
@@ -32,7 +32,6 @@ export class AuthController {
   @ApiOperation({ summary: 'Obter token de acesso para Bling' })
   @Get('blingToken/:company')
   async getBlingToken(@Param('company') company: string) {
-    await this.blingAuthService.autoRefreshToken();
     const token = await this.blingTokenService.getLastToken(company);
     return token;
   }
