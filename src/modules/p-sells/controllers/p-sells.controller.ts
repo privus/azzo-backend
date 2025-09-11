4bd2081828be8e0ca4af9693038e4bfad856f3e4
andrejvb <andre.jvb@gmail.com>
2025-09-11 12:02:49 -0300

    feat: cria rota para associação

--- src/modules/p-sells/controllers/p-sells.controller.ts
+++ src/modules/p-sells/controllers/p-sells.controller.ts
@@ -2,12 +2,13 @@ import { ApiTags, ApiOperation } from '@nestjs/swagger';
 import { Controller, Get, Query, Patch, Body, Param, Post } from '@nestjs/common';
 import { PSellsService } from '../services/p-sell.service';
 import { InstallmentsDto, UpdateSellDto } from '../dto';
+import { OmieService } from '../services/omie.service';
 
 
 @ApiTags('pSells')
 @Controller('pSells')
 export class PSellsController {
-  constructor(private readonly pSellsService: PSellsService) {}
+  constructor(private readonly pSellsService: PSellsService, private readonly omieService: OmieService) {}
 
   @ApiOperation({ summary: 'Vendas por data' })
   @Get()
@@ -19,6 +20,12 @@ export class PSellsController {
   @Get('paymentMethods')
   async getPaymentMethods() {
     return this.pSellsService.findAllPaymentMethods();
+  }
+
+  @ApiOperation({ summary: 'Atualiza códigos Omie nas vendas' })
+  @Get('omie-codes')
+  async updateOmieCodes() {
+    return this.omieService.insertOmieCod();
   }
 
   @ApiOperation({ summary: 'Sincroniza com a tabela pSell' })
