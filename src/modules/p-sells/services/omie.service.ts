1674948e66ab76a88fbe8ea2e5fc4bd985b7961e
andrejvb <andre.jvb@gmail.com>
2025-09-30 08:30:44 -0300

    fix: cria metas e metricas de progresso para vendedor

--- src/modules/p-sells/services/omie.service.ts
+++ src/modules/p-sells/services/omie.service.ts
@@ -26,7 +26,7 @@ export class OmieService {
             call: "ListarContasReceber",
             param: [
             {
-                pagina: 3,
+                pagina: 4,
                 registros_por_pagina: 100,
                 apenas_importado_api: "S",
             }
