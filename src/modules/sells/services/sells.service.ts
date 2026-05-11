3a614d69367f28a3e025b9f8cfc14598fc6785a4
andrejvb <andre.jvb@gmail.com>
2026-05-11 09:42:07 -0300

    fix: ajuste bonus semanal

--- src/modules/sells/services/sells.service.ts
+++ src/modules/sells/services/sells.service.ts
@@ -1948,16 +1948,16 @@ export class SellsService implements ISellsRepository {
       result[vendedorNome].valor_total += incrementoPedido;
     }
 
-    const vendedor1 = vendedoresMap.get(1);
-    if (vendedor1) {
-      if (!result[vendedor1]) {
-        result[vendedor1] = {
+    const vendedor2 = vendedoresMap.get(2);
+    if (vendedor2) {
+      if (!result[vendedor2]) {
+        result[vendedor2] = {
           valor_total: 0,
           pedidos: 0,
           clientes_novos: 0,
         };
       }
-      result[vendedor1].valor_total -= 30;
+      result[vendedor2].valor_total -= 30;
     }
 
     const vendedor3 = vendedoresMap.get(3);
